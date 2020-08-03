94af140277849247ca4b7988605e922d154d7aec
Ethan Chen <intervigil@gmail.com>
2020-08-03 19:08:09 +0530

    X00TD: Add assertions on TrustZone version
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@
 DEVICE_PATH := device/asus/X00TD
 
 # Assert
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := ASUS_X00TD,ASUS_X00T,X00TD,X00T
 
 # Boot animation
