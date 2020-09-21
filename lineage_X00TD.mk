5df4dcbc74bcb75364e0bef782fbf5932055737e
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-09-21 21:24:24 +0530

    X00TD: Use walleye oreo FP to bypass safetynet
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -38,10 +38,10 @@ TARGET_VENDOR_PRODUCT_NAME := asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
