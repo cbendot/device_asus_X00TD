d75c9b793eea940643be8dd981190ea7cad1efc7
Danish1999 <danish01031999@gmail.com>
2020-10-03 14:58:04 +0530

    X00TD : Add props from stock
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- properties.mk
+++ properties.mk
@@ -1,5 +1,17 @@
+# ASUS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hq.project=ZQL1650
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.is_type=5 \
+    persist.vendor.camera.mpo.disabled=1 \
+    persist.vendor.camera.rtb.enable=1 \
+    persist.vendor.camera.aec.sync=1 \
+    vendor.debug.camera.prop_dis=1 \
+    persist.vendor.camera.HAL3.enabled=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android \
@@ -8,3 +20,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.sf.lcd_density=440
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.sys.iscdfinger_998eas=1 \
+   persist.sys.fingerauto=1
+
+
