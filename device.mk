df8ae6804bff2f8d95390afe8a27a09bedc4638f
harshhaareddy <harshhaa03@gmail.com>
2021-09-13 14:16:22 +0530

    X00TD: overlay-xd: declare ```X00TD``` specs
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>
    Signed-off-by: Thoreck <maulanafajar751@gmail.com>

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-xd
 
 # Ramdisk
 PRODUCT_PACKAGES += \
