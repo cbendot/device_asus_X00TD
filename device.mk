amackpro/device_asus_X00TD-1

X00TD: add dummy keylayouts for fingerprints This is make to prevent the use key codes from Generic.kl Fingerprint gestures not yet implemented in this ROM so this files is empty

device.mk
@@ -273,7 +273,9 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/gxfp_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gxfp_input.kl \
+    $(LOCAL_PATH)/configs/keylayout/cdfinger_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/cdfinger_input.kl
 
 # Lights
 PRODUCT_PACKAGES += \

cbendot/device_asus_X00TD

Signed-off-by: cbendot <r.budhiono@gmail.com>

device.mk
@@ -51,6 +51,8 @@ PRODUCT_PACKAGES += \
 # Keylayout
  PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(DEVICE_PATH)/configs/keylayout/gxfp_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gxfp_input.kl \
+    $(DEVICE_PATH)/configs/keylayout/cdfinger_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/cdfinger_input.kl
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \