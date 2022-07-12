55b18e78c701753c30f05cc2f56ffc69e23d221e
ben863 <r.budhiono@gmail.com>
2022-07-13 00:09:36 +0700

    X00TD: device: nuke overlay-arrow local_path
    Change-Id: Id693ef5b562de9a89b9788fa3607dca53f108215
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- device.mk
+++ device.mk
@@ -13,8 +13,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
