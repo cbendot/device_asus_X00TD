57d981c8aa088116ed5c12a20ce627981f5f1296
ben863 <r.budhiono@gmail.com>
2022-08-16 19:55:22 +0700

    X00TD: BringUp SuperiorOS
    Change-Id: I6c330948a47d04e9dab7bae26d23bdafe58e4551
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X00TD.mk
+    $(LOCAL_DIR)/superior_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X00TD-userdebug
+    superior_X00TD-userdebug
 
--- superior_X00TD.mk
+++ superior_X00TD.mk
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common SuperiorOS stuff
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X00TD
+PRODUCT_NAME := superior_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
